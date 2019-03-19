2ea4cc7f76565b60675447f8db717aec354f3227
rtimi <48734019+rtimi@users.noreply.github.com>
2019-03-19 14:53:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {}
 
-config.host = process.env.HOST || "Substitute the value of URI here";
-config.authKey = process.env.AUTH_KEY || "Substitute the value of PRIMARY KEY here";
+config.host = process.env.HOST || "https://timicosmo.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "Z5DSIwwxkuKaULVOCmQlTtTDXg4nC0LcJA1YvjoxRgISDQDTq7LqyooEL4aTv9irb6jQuHB2BJmhkUJeh1OILA==";
 config.databaseId = "OrderDB";
 config.collectionId = "PurchaseOrders";
 
