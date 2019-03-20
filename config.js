65dab4941cb62496f900fed0e6d788ff93d3a00f
rtimi <48734019+rtimi@users.noreply.github.com>
2019-03-20 13:41:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {}
 
 config.host = process.env.HOST || "https://timicosmo.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "Z5DSIwwxkuKaULVOCmQlTtTDXg4nC0LcJA1YvjoxRgISDQDTq7LqyooEL4aTv9irb6jQuHB2BJmhkUJeh1OILA==";
+config.authKey = process.env.AUTH_KEY || "OQfYc4BYJ5QcOOzumu3essAThOYV3mzz1nrD2MY60TB8mYaj3n4CZghYLf92m57nB0rhi7TkaTmRck06jXbJ5A==";
 config.databaseId = "OrderDB";
 config.collectionId = "PurchaseOrders";
 
